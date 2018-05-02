d80c559cff50cacd312b8bf6ed530d4254d4e7fe
Austin Atendido <austin@hackernest.com>
2018-05-01 17:56:55 -0700

    adding compiled python is a motherfucker, resetting and pushing from master

--- blog_routing/apps/system/views.py
+++ blog_routing/apps/system/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 
+def index(request):
+    return render(request, 'blogs/index.html')
+
+def new(request):
+    response = 'placeholder to display a new form to create a new blog'
+    return HttpResponse(response)
+
+def create(request):
+    return redirect(index)
+
+def show(request, blog_id):
+    response = 'placeholder to display blog' + ' ' + blog_id
+    return HttpResponse(response, blog_id)
+
+def edit(request, blog_id):
+    response = 'placeholder to edit blog' + ' ' + blog_id
+    return HttpResponse(response, blog_id)
+
+def destroy(request, blog_id):
+    return redirect(index)
 # Create your views here.
